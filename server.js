119fb7eabbb40ddbeb677cb62b5890739b954ae5
AntonniSMoraes <antonnimoraes@gmail.com>
2025-10-23 18:58:49 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log(`JSON Server está rodando na porta ${port}`);
+});
